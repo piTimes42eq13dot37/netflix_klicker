cfd1aed18496fd4da6cb07f13964385c94180210
piTimes42eq13dot37 <107221084+piTimes42eq13dot37@users.noreply.github.com>
2022-07-06 22:22:06 +0200

    Create klicker.js

---
+++ klicker.js
@@ -0,0 +1,19 @@
+setInterval(clickNetflix, 1000);
+
+function clickNetflix(){
+	const labels = ['Nächste Folge','Intro überspringen']
+	labels.forEach(clicker)
+}
+
+function clicker(label){
+	const found = getElementByXpath("//button[contains(., '"+label+"')]") ;
+	if (found){
+		found.click();
+		console.log(new Date().toLocaleString() + " " + label);
+	}
+}
+
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
